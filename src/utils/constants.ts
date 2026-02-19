770fd91a4a02dbd0c17e47b2092ed7784b8a5a3b
RPremChand <premchand.ravuri@cognizant.com>
2026-02-19 17:54:05 +0530

    Create constants.ts with test constants and selectors

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+// src/utils/constants.ts
+
+// Test constants
+export const TEST_URL = "https://www.flipkart.com/";
+export const TIMEOUT = 5000; // in milliseconds
+
+// Selectors
+export const SELECTORS = {
+    loginButton: "button._2KpZ6l._2doB4z",
+    searchBox: "input[name='q']",
+    cartButton: "a._2P_LDn",
+    productLink: ".s1Q9rs" // Example selector for product link
+};
