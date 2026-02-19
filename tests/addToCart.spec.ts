7925b1bcc3db37370f96f8c4556c4e78a5c4ab0e
RPremChand <premchand.ravuri@cognizant.com>
2026-02-19 17:52:47 +0530

    Add add to cart test cases

---
+++ tests/addToCart.spec.ts
@@ -0,0 +1,25 @@
+describe('Add to Cart Tests', () => {
+    beforeEach(() => {
+        // Code to navigate to the product page
+    });
+
+    it('should add a product to the cart successfully', () => {
+        // Code to add product to cart
+        // Assert that the product has been added to the cart
+    });
+
+    it('should display error when adding out of stock product', () => {
+        // Code to attempt adding an out of stock product
+        // Assert that an error message is displayed
+    });
+
+    it('should keep the cart updated when adding multiple products', () => {
+        // Code to add multiple products to the cart
+        // Assert that the cart shows the correct number of products
+    });
+
+    it('should remove a product from the cart', () => {
+        // Code to add a product, then remove it from the cart
+        // Assert that the product has been removed
+    });
+});
